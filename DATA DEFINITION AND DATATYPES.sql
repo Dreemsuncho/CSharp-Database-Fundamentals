85296971e799e53caae3ab57a817ebca77676fb7
Elvis Arabadjiyski <dreemsuncho@gmail.com>
2017-09-24 14:20:48 +0300

    Trying reduce redundant space last time

--- DATA DEFINITION AND DATATYPES.sql
+++ DATA DEFINITION AND DATATYPES.sql
@@ -10,40 +10,40 @@ CREATE DATABASE Minions
 USE Minions
 
 CREATE TABLE Minions(Id INT PRIMARY KEY,
-					 Name NVARCHAR(15),
-					 Age INT)
+                     Name NVARCHAR(15),
+                     Age INT)
 
 CREATE TABLE Towns(Id INT PRIMARY KEY,
-				   Name NVARCHAR(15))
+                   Name NVARCHAR(15))
 
 
 /* Problem 3 - Alter Minions Table.
 -- Change the structure of the Minions table to have new column TownId that would be of the same type as the Id column of Towns table. Add new constraint that makes TownId foreign key and references to Id column of Towns table.
 */
 ALTER TABLE Minions
-	ADD TownId INT FOREIGN KEY REFERENCES Towns(Id)
+    ADD TownId INT FOREIGN KEY REFERENCES Towns(Id)
 
 
-/* Problem 4.	Insert Records in Both Tables
+/* Problem 4 - Insert Records in Both Tables.
 -- Populate both tables with sample records given in the table below.
-   |--------------------------------------------|	
-   | Minions					| Towns			|
-   | Id	Name	Age		TownId	| Id	Name	|
-   |--------------------------------------------|
-   | 1	Kevin	22		1		| 1		Sofia	|
-   | 2	Bob		15		3		| 2		Plovdiv	|
-   | 3	Steward	NULL	2		| 3		Varna	|
-   |--------------------------------------------|
+   |-------------------------------------|	
+   | Minions                | Towns      |
+   | Id	Name	Age  TownId | Id Name    |
+   |-------------------------------------|
+   | 1  Kevin   22   1	    | 1  Sofia	 |
+   | 2  Bob     15   3      | 2  Plovdiv |
+   | 3  Steward	NULL 2      | 3  Varna	 |
+   |-------------------------------------|
    Use only SQL queries. Submit your INSERT statements as Run skeleton, run queries & check DB.
 */
 INSERT INTO Towns (Id, Name)
-	VALUES (1, 'Sofia'),
-		   (2, 'Plovdiv'),
-		   (3, 'Varna')
-
+    VALUES (1, 'Sofia'),
+           (2, 'Plovdiv'),
+           (3, 'Varna')
+            
 INSERT INTO Minions (Id, Name, Age, TownId)
-	VALUES (1, 'Kevin', 22, 1),
-		   (2, 'Bob', 15, 3),
-		   (3, 'Steward', NULL, 2)
+    VALUES (1, 'Kevin', 22, 1),
+           (2, 'Bob', 15, 3),
+           (3, 'Steward', NULL, 2)
 
 
