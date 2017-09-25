b45e9c5c3c24abecc7eedfa2d371dbc7e6b1fe8a
Elvis Arabadjiyski <dreemsuncho@gmail.com>
2017-09-25 16:32:22 +0300

    Remove redundant space from file

--- DATA DEFINITION AND DATATYPES.sql
+++ DATA DEFINITION AND DATATYPES.sql
@@ -468,11 +468,11 @@ RESTORE DATABASE SoftUni
     • Departments: Engineering, Sales, Marketing, Software Development, Quality Assurance
     • Employees:
    |---------------------------------------------------------------------------------------|
-   | Name	                 Job Title	       Department             Hire Date	   Salary  |
+   | Name                    Job Title         Department             Hire Date	   Salary  |
    |---------------------------------------------------------------------------------------|
-   | Ivan Ivanov Ivanov	   | .NET Developer  | Software Development	| 01/02/2013 | 3500.00 |
+   | Ivan Ivanov Ivanov	   | .NET Developer  | Software Development | 01/02/2013 | 3500.00 |
    | Petar Petrov Petrov   | Senior Engineer | Engineering          | 02/03/2004 | 4000.00 |
-   | Maria Petrova Ivanova | Intern	         | Quality Assurance    | 28/08/2016 | 525.25  |
+   | Maria Petrova Ivanova | Intern          | Quality Assurance    | 28/08/2016 | 525.25  |
    | Georgi Teziev Ivanov  | CEO             | Sales                | 09/12/2007 | 3000.00 |
    | Peter Pan Pan         | Intern          | Marketing            | 28/08/2016 | 599.88  |
    |---------------------------------------------------------------------------------------|
