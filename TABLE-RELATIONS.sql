ae5bbc717f7c8870f8279e44705ab1dfd34b1aa1
Elvis Arabadjiyski <dreemsuncho@gmail.com>
2017-10-04 11:03:31 +0300

    Add next

--- TABLE-RELATIONS.sql
+++ TABLE-RELATIONS.sql
@@ -35,3 +35,48 @@ ALTER TABLE Passports
 ALTER TABLE Persons
 	ADD PRIMARY KEY (PersonID),
         FOREIGN KEY (PassportID) REFERENCES Passports(PassportID)
+
+
+/* Problem 2 - One-To-Many Relationship.
+-- Create two tables as follows. Use appropriate data types.
+   |------------------------------------|----------------------------------------|
+   |              Models                |              Manufacturers             |
+   |------------------------------------|----------------------------------------|
+   | ModelID | Name	   | ManufacturerID | ManufacturerID | Name  | EstablishedOn |
+   | 101     | X1      | 1              | 1              | BMW   | 07/03/1916    |
+   | 102     | i6      | 1              | 2              | Tesla | 01/01/2003    |
+   | 103     | Model S | 2              | 3              | Lada  | 01/05/1966    |
+   | 104     | Model X | 2              |----------------------------------------|
+   | 105     | Model 3 | 2              |
+   | 106     | Nova    | 3              |
+   |------------------------------------|
+   Insert the data from the example above. Add primary keys and foreign keys.
+*/
+CREATE TABLE Models(ModelID INT NOT NULL,
+                    Name VARCHAR(50),
+                    ManufacturerID INT NOT NULL)
+					
+CREATE TABLE Manufacturers(ManufacturerID INT NOT NULL,
+                           Name VARCHAR(50),
+                           EstablishedON DATE)
+
+INSERT INTO Models(ModelID, Name, ManufacturerID)
+	VALUES (101, 'X1', 1),
+           (102, 'i6', 1),
+           (103, 'Model S', 2),
+           (104, 'Model X', 2),	
+           (105, 'Model 3', 2),	
+           (106, 'Nova', 3)
+
+
+INSERT INTO Manufacturers (ManufacturerID, Name, EstablishedON)
+    VALUES (1, 'BMW', '07/03/1916'),
+           (2, 'Tesla', '01/01/2003'),
+           (3, 'Lada', '01/05/1966')
+
+ALTER TABLE Manufacturers
+	ADD PRIMARY KEY (ManufacturerID)
+
+ALTER TABLE Models
+    ADD PRIMARY KEY (ModelID),
+        FOREIGN KEY(ManufacturerID) REFERENCES Manufacturers(ManufacturerID)
